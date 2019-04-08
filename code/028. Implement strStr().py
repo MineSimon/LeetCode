a0afa56d6600f7b0c71e44681b56cf6d91aeae48
Simon_Chen <32009404+MineSimon@users.noreply.github.com>
2019-04-08 16:40:43 +0800

    Update 028. Implement strStr().py

--- code/028. Implement strStr().py
+++ code/028. Implement strStr().py
@@ -9,6 +9,7 @@ class Solution:
                 for j in range(1, l2):
                     if haystack[i+j] != needle[j]:
                         break
+                # 如果for循环完成结束进入else, 如果中途结束则不执行else
                 else:
                     return i
         return -1
